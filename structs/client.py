52e91f499141481007ac8d4f1454c31bfff288b1
Kozheen Taher Esa <esa.taheresa@gmail.com>
2024-12-10 16:30:47 +0200

    update struct

--- structs/client.py
+++ structs/client.py
@@ -11,6 +11,27 @@ Make sure to add re-exports to __init__.py for new additions.
 
 @dataclass_json
 @dataclass
+class NDS_Group:
+	"""
+	A class used to represent a Group (i.e. a chat room). NDS does not need much info about groups.
+
+	Attributes
+	----------
+	group_id : str
+		The unique identifier of the group.
+	name : str
+		The name of the group.
+	leader_ip : str
+		The IP address of the group leader, which is a Node.
+	"""
+
+	group_id: str
+	name: str
+	leader_ip: str 
+
+
+@dataclass_json
+@dataclass
 class Node:
 	"""
 	A class used to represent any Node (i.e. another client).
