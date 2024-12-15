fd9c195cf64103ab2202ec96f8bfbd1d3eb0a4f0
Kozheen Taher Esa <esa.taheresa@gmail.com>
2024-12-15 02:05:40 +0200

    overseer refresh on change

--- client/service/overseer.py
+++ client/service/overseer.py
@@ -4,6 +4,7 @@ import logging
 import threading
 import time
 
+
 def start_overseer(app):
 	"""Starts overseeing thread"""
 	if app.overseer:
@@ -14,7 +15,7 @@ def start_overseer(app):
 	logging.info("Starting overseer.")
 	app.overseer_counter += 1
 	app.overseer = threading.Thread(
-		target=overseer_thread, args=(app, app.overseer_counter,), daemon=True
+		target=overseer_thread, args=(app, app.overseer_counter), daemon=True
 	)
 	app.overseer.start()
 
@@ -22,6 +23,9 @@ def start_overseer(app):
 def overseer_thread(app, ov_id: int):
 	"""Thread to monitor hearbeats from followers, deleting ones not active"""
 	try:
+		previous_state = (
+			set(app.active_group.peers.keys()) if app.active_group else set()
+		)
 		while True:
 			logging.info(f"OV: Overseeing at {ov_id}.")
 			if ov_id in app.overseer_kill_flags:
@@ -34,22 +38,36 @@ def overseer_thread(app, ov_id: int):
 			):
 				raise InterruptedError
 
-			nodes_to_delete = []
 			with app.overseer_lock:
+
+				nodes_to_delete = []
 				for node_id in app.last_node_response.keys():
-					new_val = app.last_node_response[node_id] + 1
-					if new_val > app.overseer_cycles_timeout:
+					app.last_node_response[node_id] = (
+						app.last_node_response[node_id] + 1
+					)
+					if app.last_node_response[node_id] > app.overseer_cycles_timeout:
 						# If have not received heartbeat from group leader in 10 cycles, delete Group
 						nodes_to_delete.append(node_id)
-					else:
-						app.last_node_response[node_id] = new_val
 
 				for node_id in nodes_to_delete:
 					del app.last_node_response[node_id]
 					del app.active_group.peers[node_id]
-					logging.info(f"OV: Node {node_id} deleted -- no heartbeat from node.")
+					logging.info(
+						f"OV: Node {node_id} deleted -- no heartbeat from node."
+					)
 
+			updated_state = set(app.active_group.peers.keys())
+			added_peers = updated_state - previous_state
+			removed_peers = previous_state - updated_state
+
+			if added_peers or removed_peers:
+				logging.info(
+					"OV: State change, Added {added_peers}, Removed {removed_peers}"
+				)
 				app.networking.refresh_group(app.active_group)
+
+			previous_state = updated_state
+
 			time.sleep(app.overseer_interval)
 	except Exception as e:
 		logging.error(f"EXC: Critical error, Overseer failed {e}")
